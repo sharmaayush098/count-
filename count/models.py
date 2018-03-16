ef46f0eaca4381f473a0833e1db3216dda0bec86
ayush <sharmaayush098@gmail.com>
2018-03-16 18:19:24 +0530

    work in progress

--- count/models.py
+++ count/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.http import HttpResponse
+from django.shortcuts import render
 
 
 class Count(models.Model):
     increment = models.IntegerField(default=0)
 
 
+class DataStored(models.Model):
+    username = models.CharField(max_length=100)
+    time = models.TimeField()
+    action_type = models.CharField(max_length=100, blank=True)
+    count_id = models.ForeignKey(Count)
 
+    def __str__(self):
+        return self.username + str(self.time)
--- count/views.py
+++ count/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Count, Count
+from .models import Count, DataStored
 
 
 @csrf_exempt
@@ -28,12 +28,30 @@ def count_number(request, count_id=None):
             count_obj.increment += 1
             count_obj.save()
             counter_val = Count.objects.get(id=int(count_id)).increment
+            user = User.objects.get(id=int(count_id))
+            position = user
+            username = user.username
+            time = datetime.datetime.now().strftime('%H:%M:%S')
+            current_time = str(time)
+            storage = DataStored.objects.create(username=username, time=current_time, count_id=position,
+                                                action_type="increment")
+
 
         else:
             count_obj = Count.objects.create(increment=1)
             counter_val = count_obj.increment
+            user = User.objects.get(id=int(count_id))
+            position = user
+            username = user.username
+            time = datetime.datetime.now().strftime('%H:%M:%S')
+            current_time = str(time)
+            storage = DataStored.objects.create(username=username, time=current_time, count_id=position,
+                                                action_type="increment")
+            storage.save()
         return HttpResponse(
-            content_type="application/json", content=json.dumps({"count": counter_val})
+            content_type="application/json", content=json.dumps({"count": counter_val, "time": current_time,
+                                                                 "count_id": position,
+                                                                 "user": username, "action": "increment"})
         )
 
 
@@ -56,26 +74,25 @@ def count_decrease(request, count_id=None):
             count_obj = Count.objects.create(increment=-1)
             count_value = count_obj.increment
         return HttpResponse(
-                content_type='application/json', content=json.dumps({"count": count_value})
-            )
+            content_type='application/json', content=json.dumps({"count": count_value})
+        )
 
 
 @csrf_exempt
-def detail(request, user_id=None):
+def detail(request, user_id=None,):
     if request.method == "GET":
         user = User.objects.get(id=int(user_id))
-        time = datetime.datetime.now()
-        return render(request, "increment_page.html", {"user": user, "time": time})
+
+        return render(request, "increment_page.html", {"user": user})
     elif request.method == "POST":
         user = User.objects.get(id=int(user_id))
         username = user.username
 
-        time = datetime.datetime.now()
+        time = datetime.datetime.now().strftime('%H:%M:%S')
         current_time = str(time)
 
-        user.save()
-
-        return HttpResponse(content_type='application/json', content=json.dumps({"user": username, 'time': current_time}))
+        return HttpResponse(content_type='application/json', content=json.dumps({"user": username,
+                                                                                 "time": current_time}))
 
 
 
