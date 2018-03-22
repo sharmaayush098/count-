d0fe4b8063606f6b9e57ca56f6d5968a7121ca67
ayush <sharmaayush098@gmail.com>
2018-03-22 17:19:08 +0530

    Complete Counter App

--- practice/settings.py
+++ practice/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': 'practice_count',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+    },
+
 }
 
 
